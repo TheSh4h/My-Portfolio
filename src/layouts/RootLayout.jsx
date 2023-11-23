03d500b4ce1560cd4b1102664f384a70b0aa8557
TheSh4h <hh.haseebshah@gmail.com>
2023-11-23 22:27:37 +0500

    about done

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,9 +1,11 @@
+import About from "../components/About/About";
 import Navbar from "../components/Navbar/Navbar";
 
 const RootLayout = () => {
     return ( 
         <>
             <Navbar />
+            <About />
         </>
      );
 }
