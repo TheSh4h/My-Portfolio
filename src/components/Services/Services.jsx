6fae219ce9159b2634177542fd899a7ff4061df4
TheSh4h <hh.haseebshah@gmail.com>
2023-11-25 17:23:00 +0500

    links added

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,23 +1,45 @@
+import { Link } from "react-router-dom";
+
 const Services = () => {
     return ( 
         <div className="container mx-auto p-5 lg:p-0 my-28 text-start">
-            <h1 className="text-xl font-bold text-gray-800">Services I Offer</h1>
-            <div className="flex flex-col md:flex-row justify-evenly items-center my-5">
-                <div className="flex justify-evenly space-x-5">
-                    <img width="64" height="64" src="https://img.icons8.com/external-nawicon-flat-nawicon/64/external-copywriting-seo-and-marketing-nawicon-flat-nawicon.png" alt="external-copywriting-seo-and-marketing-nawicon-flat-nawicon"/>
-                    <div className="w-64 h-10">
-                        <h1 className="text-lg font-semibold">Copywriting</h1>
-                        <p className="text-sm text-gray-500">Bring in huge revenue through your emails, sale pages or landing pages by utilizing my persuasive copywriting skill</p>
-                    </div>
-                </div>
+            
+            <h1 className="text-xl font-bold text-gray-800">My Services</h1>
 
-                <div className="flex space-x-5 mt-10 md:mt-0">
-                    <img width="70" height="70" src="https://img.icons8.com/ios/100/laptop-coding.png" alt="laptop-coding"/>
-                    <div className="w-64 h-10">
-                        <h1 className="text-lg font-semibold">Web Development</h1>
-                        <p className="text-sm text-gray-500">Get your high converting website and landing pages built now and give your brand a unique identity</p>
+            <div className="flex flex-col md:flex-row justify-evenly items-center my-5">
+                <Link to='/copywriting' className="hover:bg-gray-100 transition duration-300 rounded-lg shadow-md">
+                    <div className="flex items-center space-x-5 p-3">
+                        <img
+                            className="w-16 h-16 object-cover"
+                            src="https://img.icons8.com/external-nawicon-flat-nawicon/64/external-copywriting-seo-and-marketing-nawicon-flat-nawicon.png"
+                            alt="Copywriting Icon"
+                        />
+                        <div className="flex flex-col w-64">
+                            <h1 className="text-lg font-semibold">Copywriting</h1>
+                            <p className="text-sm text-gray-500">
+                            Bring in huge revenue through your emails, sales pages, or landing pages by utilizing my persuasive copywriting skills.
+                            </p>
+                        </div>
                     </div>
-                </div>
+
+                </Link>
+
+               <Link to='/web' className="hover:bg-gray-100 transition duration-300 rounded-lg shadow-md">
+                    <div className="flex items-center space-x-5 p-3">
+                        <img
+                            className="w-16 h-16 object-cover"
+                            src="https://img.icons8.com/ios/100/laptop-coding.png"
+                            alt="Web Development Icon"
+                        />
+                        <div className="flex flex-col w-64">
+                            <h1 className="text-lg font-semibold">Web Development</h1>
+                            <p className="text-sm text-gray-500">
+                            Get your high-converting website and landing pages built now and give your brand a unique identity.
+                            </p>
+                        </div>
+                    </div>
+               </Link>
+
             </div>
         </div>
      );
