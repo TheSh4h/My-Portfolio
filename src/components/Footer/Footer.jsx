a1d07f874c6ff2b5e1c6b4827e41fc3c768841c9
TheSh4h <hh.haseebshah@gmail.com>
2023-11-25 10:44:13 +0500

    footer done

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return ( 
+        <div className="bg-gray-800 text-start">
+            <div className="container mx-auto">
+                <h1 className="font-bold text-white text-xl p-10">Copyright © All rights are reserved</h1>
+            </div>
+        </div>
+     );
+}
+ 
+export default Footer;
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,4 +1,6 @@
 import About from "../components/About/About";
+import Contact from "../components/Contact/Contact";
+import Footer from "../components/Footer/Footer";
 import Interest from "../components/Interest/Interest";
 import Navbar from "../components/Navbar/Navbar";
 import Services from "../components/Services/Services";
@@ -10,6 +12,10 @@ const RootLayout = () => {
             <About />
             <Services />
             <Interest />
+            <Contact />
+            <footer>
+                <Footer />
+            </footer>
         </>
      );
 }
