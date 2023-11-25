9cf6ef32de1ba4c2a23abe89a2c2fa96ae0c98d9
TheSh4h <hh.haseebshah@gmail.com>
2023-11-25 10:47:58 +0500

    date added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,12 @@
 const Footer = () => {
+
+    let currentDate = new Date();
+    let year = currentDate.getFullYear();
+
     return ( 
         <div className="bg-gray-800 text-start">
             <div className="container mx-auto">
-                <h1 className="font-bold text-white text-xl p-10">Copyright © All rights are reserved</h1>
+                <h1 className="font-bold text-white text-xl p-10">Copyright © {year}. All rights are reserved</h1>
             </div>
         </div>
      );
