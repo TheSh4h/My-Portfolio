6bc056b0d7d2b4d0745beb7ef0a2ae547393bcc8
TheSh4h <hh.haseebshah@gmail.com>
2023-11-25 21:30:40 +0500

    copywriting page done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     let year = currentDate.getFullYear();
 
     return ( 
-        <div className="bg-gray-800 text-start">
+        <div className="bg-gray-800 text-start relative bottom-0">
             <div className="container mx-auto">
                 <h1 className="font-bold text-white text-xl p-10">Copyright © {year}. All rights are reserved</h1>
             </div>
--- src/pages/copywriting/Copywriting.jsx
+++ src/pages/copywriting/Copywriting.jsx
@@ -1,7 +1,41 @@
 const Copywriting = () => {
     return ( 
-        <div>
-            
+        <div className="p-5 h-screen">
+            <div className="container mx-auto my-10">
+                <div className="flex justify-center items-center space-x-5">
+                    <img src="/swords.png" alt=" swords" className="w-14 h-14" />
+                    <h1 className="text-xl font-bold py-10">Copy Prepared for Battle - Check 'em out</h1>
+                </div>
+                <a href="https://docs.google.com/document/d/1PB5e_vCGzknwa1733f3HAMfH55nohAw4iPiDygOB9OY/edit?usp=sharing" target='_blank'>
+                    <div 
+                    className="flex justify-between text-start py-8
+                    border-t border-gray-800 p-4 hover:bg-gray-100 hover:shadow-md
+                    transition duration-300">
+                        <h1 className="text-md font-bold">Web Development Service</h1>
+                        <h1 className="text-md font-semibold text-gray-800">Not closing deals? Become Authentic…</h1>
+                    </div>
+                </a>
+
+                <a href="https://docs.google.com/document/d/1s7adxY2oLg3wTNfs9f6TvCF_2tjAl3Mnjq2G1fwJw4c/edit?usp=sharing" target='_blank'>
+                    <div 
+                    className="flex justify-between text-start py-8
+                    border-t border-gray-800 p-4 hover:bg-gray-100 hover:shadow-md
+                    transition duration-300">
+                        <h1 className="text-md font-bold">Protein Supplement</h1>
+                        <h1 className="text-md font-semibold text-gray-800">Eating less = Big Muscles…?</h1>
+                    </div>
+                </a>
+
+                <a href="https://docs.google.com/document/d/1J3b2dgeuGn8ThHzQfoRcOyvljhK8DhKdWCz2t0yckDQ/edit?usp=sharing" target='_blank'>
+                    <div 
+                    className="flex justify-between text-start py-8
+                    border-t border-gray-800 p-4 hover:bg-gray-100 hover:shadow-md
+                    transition duration-300">
+                        <h1 className="text-md font-bold">Solar Panels</h1>
+                        <h1 className="text-md font-semibold text-gray-800">Save Up To 79% on Your Electricity Bills</h1>
+                    </div>
+                </a>
+            </div>
         </div>
      );
 }
