d105bca1310a651d89b4facd0b9fd113a19c4e30
TheSh4h <hh.haseebshah@gmail.com>
2023-11-25 16:43:24 +0500

    fixed

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,15 +4,15 @@ const Contact = () => {
 
             <div className="flex flex-col text-start">
                 <h1 className="font-bold text-xl text-gray-800">Don't be Shy! Hit me Up! 👇</h1>
-                <div className="flex m-5">
-                    <img src="/mail.png" alt="mail" className="h-[40px] w-[40px]" />
-                    <div className="text-start ml-2">
-                        <a href="mailto:hh.haseebshah@gmail.com" target="_blank">
-                            <h1 className="font-semibold text-gray-800 text-sm">Mail</h1>
-                            <p className="text-gray-500 text-xs">hh.haseebshah@gmail.com</p>
-                        </a>
+                <a href="mailto:hh.haseebshah@gmail.com" target="_blank">
+                    <div className="flex m-5">
+                        <img src="/mail.png" alt="mail" className="h-[40px] w-[40px]" />
+                        <div className="text-start ml-2">
+                                <h1 className="font-semibold text-gray-800 text-sm">Mail</h1>
+                                <p className="text-gray-500 text-sm">hh.haseebshah@gmail.com</p>
+                        </div>
                     </div>
-                </div>
+                </a>
 
                 <h1 className="font-bold text-xl text-gray-800">Connect with me 👇</h1>
 
