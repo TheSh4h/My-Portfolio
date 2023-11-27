b2be5eea95e5d1f5483d0f3ace5179e16ed34134
TheSh4h <hh.haseebshah@gmail.com>
2023-11-27 16:55:23 +0500

    page fixed

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,9 @@
 const Contact = () => {
     return ( 
-        <div className="container mx-auto p-5 lg:p-0 my-10">
+        <div className="container mx-auto p-5 lg:p-0 my-10 flex flex-col justify-center items-center">
 
-            <div className="flex flex-col text-start">
-                <h1 className="font-bold text-xl text-gray-800">Don't be Shy! Hit me Up! 👇</h1>
+            <div className="flex flex-col justify-center items-center">
+                <h1 className="font-bold text-3xl text-gray-800">Contact Me 👇</h1>
                 <a href="mailto:hh.haseebshah@gmail.com" target="_blank">
                     <div className="flex m-5">
                         <img src="/mail.png" alt="mail" className="h-[40px] w-[40px]" />
@@ -14,7 +14,7 @@ const Contact = () => {
                     </div>
                 </a>
 
-                <h1 className="font-bold text-xl text-gray-800">Connect with me 👇</h1>
+                {/* <h1 className="font-bold text-xl text-gray-800">Connect with me 👇</h1> */}
 
                <div className="flex m-5 space-x-5">
                     <a href="https://www.instagram.com/thepitchsmith/" target="_blank">
